e60f9dee4b6214f0d43e0d9ab0292b9f190f692f
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2021-11-30 22:24:56 +0500

    Added sepia and negative

--- Main.cs
+++ Main.cs
@@ -27,7 +27,6 @@ namespace MyPhotoshop
                     var g = Pixel.Trim((pixel.G * 255 * 100 - 128 * parameters.Coefficient) / (100 - parameters.Coefficient) / 255);
                     var b = Pixel.Trim((pixel.B * 255 * 100 - 128 * parameters.Coefficient) / (100 - parameters.Coefficient) / 255);
                     return new Pixel(r, g, b);
-
                 }));
             window.AddFilter(new PixelFilter<EmptyParameters>("Оттенки серого",
                 (pixel, parameters) =>
@@ -35,6 +34,19 @@ namespace MyPhotoshop
                     var brightness = (0.299 * (pixel.R)) +
                               0.587 * (pixel.G) + 0.114 * (pixel.B);
                     return new Pixel(brightness, brightness, brightness);
+                }));
+            window.AddFilter(new PixelFilter<EmptyParameters>("Сепия",
+                (pixel, parameters) =>
+                {
+                    var r = Pixel.Trim((0.393 * (pixel.R)) + 0.769 * (pixel.G) + 0.189 * (pixel.B));
+                    var g = Pixel.Trim((0.349 * (pixel.R)) + 0.686 * (pixel.G) + 0.168 * (pixel.B));
+                    var b = Pixel.Trim((0.272 * (pixel.R)) + 0.534 * (pixel.G) + 0.131 * (pixel.B));
+                    return new Pixel(r, g, b);
+                }));
+            window.AddFilter(new PixelFilter<EmptyParameters>("Негатив",
+                (pixel, parameters) =>
+                {
+                    return new Pixel(1 - pixel.R, 1 - pixel.G, 1 - pixel.B);
                 }));
 
             window.AddFilter(new TransformFilter("Отразить по горизонтали", size => size,
