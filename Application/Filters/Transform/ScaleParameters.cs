cbdbea1f96b68acd83c3a23758593a5aae13f866
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2022-01-10 16:32:22 +0500

    ScaleName Parameters fixed

--- Application/Filters/Transform/ScaleParameters.cs
+++ Application/Filters/Transform/ScaleParameters.cs
@@ -7,9 +7,9 @@ namespace MyPhotoshop
 {
     public class ScaleParameters: IParameters
     {
-        [ParameterData(Name = "Ширина (в процентах)", MaxValue = 2000, MinValue = 1, Increment = 1, DefaultValue = 500)]
+        [ParameterData(Name = "Ширина (в пикселях)", MaxValue = 2000, MinValue = 1, Increment = 1, DefaultValue = 500)]
         public double ScaleX { get; set; }
-        [ParameterData(Name = "Высота (в процентах)", MaxValue = 2000, MinValue = 1, Increment = 1, DefaultValue = 500)]
+        [ParameterData(Name = "Высота (в пикселях)", MaxValue = 2000, MinValue = 1, Increment = 1, DefaultValue = 500)]
         public double ScaleY { get; set; }
     }
 }
